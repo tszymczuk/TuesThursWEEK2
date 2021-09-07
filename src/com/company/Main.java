60d2c190b3e4df36243c1318cddeb67e4965631b
Tom Szymczuk <tszymczuk@student.bridgew.edu>
2021-09-07 10:17:56 -0400

    Dogyears

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    var reader = new Scanner(System.in);
+        System.out.print("How old are you?:");
+        var age = reader.nextFloat();
+        var dogyears = age*7;
+        System.out.println("Wow! You are "+age+ " that is "+dogyears+ " in dog years");
     }
 }
